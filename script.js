98a9caa68f6dc358d8aa789644221041e0492e5e
Luca Belardinelli <belardinelliluca@gmail.com>
2024-12-18 16:12:59 +0100

    [fizzBuzz]

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 for ( let i =1; i <= 100; i++){
-    
 
-    if (i % 3 === 0 ){
+    // SE I è DIVISIBILE SIA PER 3 CHE PER 5
+    if (i % 3 === 0 && i % 5 === 0 ){
+        console.log("FizzBuzz");
+    }
+    
+    // SE I è DIVISIBILE PER 3
+    else if (i % 3 === 0 ){
         console.log("Fizz");
     }
 
+    // SE I è DIVISIBILE PER 5
     else if (i % 5 === 0 ){
         console.log("Buzz");
     }
 
 
-    else if (i % 3 === 0 && i % 5 === 0 ){
-        console.log("FizzBuzz");
-    }
-
     else 
-    console.log(i)
+       console.log(i)
 
-      
+    
+  
 }
 
